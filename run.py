fc405818706ecee217b579dd9e86224026dedc05
Bjorn Bishop <bjornbishop@gmail.com>
2024-07-24 09:22:35 +0000

    adjusted validate_data function to convert strings into int.

--- run.py
+++ run.py
@@ -41,13 +41,12 @@ def validate_data(values):
     """
 
     try: 
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f'Eactly 6 values required, you provided {len(values)}'
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again\n')
-
-    print(values)
 
 get_sales_data()
